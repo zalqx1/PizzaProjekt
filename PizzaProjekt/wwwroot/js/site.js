51537ed7c20d3c7eb74d6c21461a7f036589bb06
colin.pfeilmaier <colin.pfeilmaier@NB1506-17>
2023-12-04 12:54:33 +0100

    feat(configurator): Expand template, add styling, add js for cart and buttons

--- PizzaProjekt/wwwroot/js/site.js
+++ PizzaProjekt/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+const items = document.querySelectorAll(".sidebar__item");
+
+items.forEach(item => {
+    console.log(item);
+    item.addEventListener('click', function () {
+        
+        if (this.classList.contains("active")) {
+            this.classList.remove("active");
+        } else {
+            this.classList.add("active");
+        }
+    })
+})
+
+const PriceButton = document.querySelector(".configurator__price-button");
+
+PriceButton.addEventListener("click", function () {
+    PriceButton.querySelector("svg").classList.add("berg");
+    
+    const addCart = async () => {
+        await setTimeout(7000);
+        console.log("Waited 7s");
+
+        const Cart = document.querySelector("header .header__cart");
+        Cart.classList.add("cartAdded");
+    };
+    addCart();
+})
