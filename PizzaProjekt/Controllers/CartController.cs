39f3f18a1963753ff1b6efc0770f6335ab686812
Asim Jugo <asimjugo08@gmail.com>
2024-02-11 20:40:22 +0100

    refactor: move addToCart Action

--- PizzaProjekt/Controllers/CartController.cs
+++ PizzaProjekt/Controllers/CartController.cs
@@ -4,16 +4,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using PizzaProjekt.Extensions;
+using PizzaProjekt.Services;
 
 namespace PizzaProjekt.Controllers
 {
     public class CartController : Controller
     {
+        private readonly CartService _cartService;
+
+        public CartController(CartService cartService)
+        {
+            _cartService = cartService;
+        }
         public IActionResult Index()
         {
             List<string> cartItems = HttpContext.Session.Get<List<string>>("Cart");
 
             return View(cartItems);
+        }
+
+        [HttpPost]
+        public IActionResult addToCart()
+        {
+            var formData = HttpContext.Request.Form;
+            var selectedItems = _cartService.ProcessCart(formData);
+            var existingCart = HttpContext.Session.Get<List<string>>("Cart");
+            
+            if (existingCart == null)
+            {
+                // If the cart session does not exist, set it with the selected items
+                HttpContext.Session.Set("Cart", selectedItems);
+            }
+            else
+            {
+                // If the cart session exists, add the selected items to it
+                existingCart.AddRange(selectedItems);
+                HttpContext.Session.Set("Cart", existingCart);
+            }
+
+
+            return RedirectToAction("Index", "Home");
         }
         public IActionResult Checkout()
         {
--- PizzaProjekt/Controllers/HomeController.cs
+++ PizzaProjekt/Controllers/HomeController.cs
@@ -8,7 +8,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using PizzaProjekt.Repositories;
-using PizzaProjekt.Extensions;
 
 namespace PizzaProjekt.Controllers
 {
@@ -16,32 +15,18 @@ namespace PizzaProjekt.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IngredientsRepository _ingredientsRepository;
-        private readonly CartService _cartService;
 
 
-        public HomeController(ILogger<HomeController> logger, IngredientsRepository ingredientsRepository, CartService cartService)
+        public HomeController(ILogger<HomeController> logger, IngredientsRepository ingredientsRepository)
         {
             _logger = logger;
             _ingredientsRepository = ingredientsRepository;
-            _cartService = cartService;
         }
 
         public IActionResult Index()
         {
             var ingredients = _ingredientsRepository.GetAllIngredients();
             return View(ingredients);
-        }
-
-        [HttpPost]
-        public IActionResult addToCart()
-        {
-            var formData = HttpContext.Request.Form;
-
-            var selectedItems = _cartService.ProcessCart(formData);
-
-            HttpContext.Session.Set("Cart", selectedItems);
-
-            return RedirectToAction("Index", "Home");
         }
 
         public IActionResult Privacy()
